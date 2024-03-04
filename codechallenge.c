b4dfd87b19b586658702d956a0e13348aaf835d2
hilqia_kenda <hilqiakenda0@gmail.com>
2024-03-04 12:12:01 +0300

    add a new file codechallenge.c

---
+++ codechallenge.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <math.h>
+
+
+int main() {
+
+	double a;
+	double b;
+
+	printf("This Apps will calculate the hypoteunus value\n"); //The message to display regarding our app
+
+	printf("Enter your first value: \n"); //Request line command of the first value
+	scanf("%lf", &a);
+	
+	printf("Enter your second value: \n"); //Request for the second value
+		scanf("%lf", &b); //lf double format characterr
+
+	double c = sqrt((a * b) + (b * b));
+
+	printf("The hypoteunus: %f\n", c); // the result of our hypoteunus value	
+
+	return 0;
+
+}
